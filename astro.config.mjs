7cb60e8eab1ccf893c4fc17063f7de792b826a03
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2024-05-23 03:37:27 +0700

    build: setup :money_with_wings:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
 
-import sitemap from '@astrojs/sitemap';
+const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: `${SITE_URL}`,
+  integrations: [mdx(), sitemap(), react()],
+})
